15505922ff954e36892b34590bbfdbd38514d219
kwanok <cloq@kakao.com>
2022-01-06 18:28:20 +0900

    unmarshal

--- json/main.go
+++ json/main.go
@@ -15,10 +15,28 @@ func main() {
 		Name: "KNOH",
 		Age:  25,
 	}
-	bytes, err := json.Marshal(user)
+	bytes, err := json.MarshalIndent(user, "", "  ")
 	if err != nil {
 		return
 	}
+
 	fmt.Println(bytes)
+	// [123 34 110 97 109 101 34 58 34 75 78 79 72 34 44 34 97 103 101 34 58 50 53 125]
+
 	fmt.Println(string(bytes))
+	/*{
+		"name": "KNOH",
+		"age": 25
+	}*/
+
+	var user1 User
+
+	err = json.Unmarshal(bytes, &user1)
+	if err != nil {
+		return
+	}
+
+	fmt.Println(user1.Age)
+	fmt.Println(user1.Name)
+
 }

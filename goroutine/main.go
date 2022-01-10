9a109ccb7463c6d1bf7059d3c519118d722acab4
kwanok <cloq@kakao.com>
2022-01-10 17:54:54 +0900

    waitgroup

--- goroutine/main.go
+++ goroutine/main.go
@@ -2,26 +2,37 @@ package main
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
+
+var wg sync.WaitGroup
 
 func 함수() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("함수():", i)
 		time.Sleep(500 * time.Millisecond)
 	}
+
+	wg.Done()
 }
 
 func 또다른함수() {
 	for i := 0; i < 10; i++ {
 		fmt.Println("또다른함수():", i)
-		time.Sleep(500 * time.Millisecond)
+		time.Sleep(400 * time.Millisecond)
 	}
+
+	wg.Done()
 }
 
 func main() {
+	wg.Add(1)
 	go 함수()
+	wg.Add(1)
 	go 또다른함수()
 
-	time.Sleep(3 * time.Second)
+	wg.Wait()
+
+	// time.Sleep(3 * time.Second)
 }
